c822989b0bcc69d6430dbd0bf4f557c8d6601b2c
Sabil49 <md.sabeel10@gmail.com>
2024-09-25 17:08:06 +0530

    chat appliction in next js, graphql and grafbase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app'
+import { ApolloProviderWrapper } from '../components/apollo-provider-wrapper'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps) {
+    return (
+        <SessionProvider session={session}>
+            <ApolloProviderWrapper>
+                <Component {...pageProps} />
+            </ApolloProviderWrapper>
+        </SessionProvider>
+    )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="bg-[#131316]">
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
